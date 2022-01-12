88642a1db7be31630096279330ecc08379ba48af
kalulica <latinovicluka.77@gmail.com>
2022-01-12 20:16:15 +0100

    zadatak 4

--- create-post.php
+++ create-post.php
@@ -1,2 +1,3 @@
-<?php 
+<?php
 include 'header.php';
+include 'db.php';
--- header.php
+++ header.php
@@ -36,8 +36,8 @@
 
         <div class="blog-header">
             <div class="container">
-                <h1 class="blog-title">The Bootstrap Blog</h1>
-                <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+                <h1 class="blog-title">Malo aktuelnosti </h1>
+                <p class="lead blog-description">Sta se prica po carsiji - saznajte</p>
             </div>
         </div>
     </header>
